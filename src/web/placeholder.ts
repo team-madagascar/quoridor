3aab1236f20e6ab141613bc12ff685b1e15851f7
a-mountain <frostnext2@gmail.com>
2021-10-04 14:35:39 +0300

    Set up webpack

--- src/web/placeholder.ts
+++
@@ -1,8 +0,0 @@
-import {GameFacade} from '../domain/game-facade';
-
-// const currentClient = new WebClient();
-// const opponentClient = getOpponentClient();
-//
-// (async () => {
-//   await GameFacade.startGame(currentClient, opponentClient);
-// })();
