456aef9d9b8617e9dec1dc506efd58fca304fef4
eve-va <yeva.vakhtina1@gmail.com>
2021-10-15 16:42:07 +0300

    Fixed coordinates

--- src/web/index.ts
+++ src/web/index.ts
@@ -3,28 +3,26 @@ import {emitter, PLAYER_ACTION_EVENT} from './emitter';
 import {Point} from '../domain/core/point';
 import {Direction} from '../domain/core/point';
 
-console.log('dhjjfjvjf');
-
 const rows = 17;
 const columns = 17;
 const board: Array<Array<Cell>> = [];
 
 class Cell {
-  x: number;
-  y: number;
+  column: number;
+  row: number;
   value: number;
   wallv: boolean;
   wallh: boolean;
 
   constructor(
-    x: number,
-    y: number,
+    column: number,
+    row: number,
     value: number,
     wallv: boolean,
     wallh: boolean
   ) {
-    this.x = x;
-    this.y = y;
+    this.column = column;
+    this.row = row;
     this.value = value;
     this.wallv = wallv;
     this.wallh = wallh;
@@ -32,10 +30,10 @@ class Cell {
 }
 
 export const renderBoard = (
-  px1: number,
-  py1: number,
-  px2: number,
-  py2: number
+  column1: number,
+  row1: number,
+  column2: number,
+  row2: number
 ) => {
   for (let r = 0; r < rows; r++) {
     board[r] = [];
@@ -48,8 +46,8 @@ export const renderBoard = (
     }
   }
 
-  board[px1][py1].value = 1;
-  board[px2][py2].value = 2;
+  board[row1][column1].value = 1;
+  board[row2][column2].value = 2;
 
   const gameContainer = document.getElementById('game_container');
   gameContainer!.innerHTML = '';
@@ -67,17 +65,17 @@ export const renderBoard = (
       if (cell.wallv) addСlass = 'wallv';
 
       if (c % 2 !== 0 && r % 2 !== 0) {
-        content += `<div class='cell ${addСlass}' id='cell11' data-x='${c}'
-        data-y='${r}'></div>`;
+        content += `<div class='cell ${addСlass}' id='cell11' data-column='${c}'
+        data-row='${r}'></div>`;
       } else if (c % 2 === 0 && r % 2 !== 0) {
-        content += `<div class='cell ${addСlass}' id='cell01' data-x='${c}'
-        data-y='${r}'></div>`;
+        content += `<div class='cell ${addСlass}' id='cell01' data-column='${c}'
+        data-row='${r}'></div>`;
       } else if (c % 2 !== 0 && r % 2 === 0) {
-        content += `<div class='cell ${addСlass}' id='cell10' data-x='${c}'
-        data-y='${r}'></div>`;
+        content += `<div class='cell ${addСlass}' id='cell10' data-column='${c}'
+        data-row='${r}'></div>`;
       } else {
-        content += `<div class='cell ${addСlass}' id='cell' data-x='${c}'
-        data-y='${r}'></div>`;
+        content += `<div class='cell ${addСlass}' id='cell' data-column='${c}'
+        data-row='${r}'></div>`;
       }
     }
     content += '</div>';
@@ -89,74 +87,77 @@ document.getElementById('game_container')?.addEventListener('click', e => {
   const target: HTMLElement = e.target as HTMLElement;
   const options: Array<HTMLElement> = [];
   let direction;
-  const x = +(target.getAttribute('data-x') as string);
-  const y = +(target.getAttribute('data-y') as string);
+  const column = +(target.getAttribute('data-column') as string);
+  const row = +(target.getAttribute('data-row') as string);
 
-  let x1;
-  let ox1;
-  let x2;
-  let ox2;
-  let x3;
-  let y1;
-  let oy1;
-  let y2;
-  let oy2;
-  let y3;
+  const column1 = column - 1;
+  const column2 = column + 1;
+  const column3 = column - 2;
+  const column4 = column + 2;
+  const row1 = row - 1;
+  const row2 = row + 1;
+  const row3 = row - 2;
+  const row4 = row + 2;
 
-  if (x > 0) x1 = x - 1;
-  if (x < 16) x2 = x + 1;
-  if (y > 0) y1 = y - 1;
-  if (y < 16) y2 = y + 1;
-
-  if (x > 1) ox1 = x - 2;
-  if (x < 15) ox2 = x + 2;
-  if (y > 1) oy1 = y - 2;
-  if (y < 15) oy2 = y + 2;
-
-  if (x < 14) x3 = x + 2;
-  if (y < 14) y3 = y + 2;
+  let wall1;
+  let wall2;
+  let wall3;
+  let wall4;
 
   if (
     target.classList.contains('player1') ||
     target.classList.contains('player2')
   ) {
-    const wall1 = document.querySelector(
-      `.cell[data-x='${x}'][data-y='${y1}']`
-    );
-    const wall2 = document.querySelector(
-      `.cell[data-x='${x}'][data-y='${y2}']`
-    );
-    const wall3 = document.querySelector(
-      `.cell[data-x='${x1}'][data-y='${y}']`
-    );
-    const wall4 = document.querySelector(
-      `.cell[data-x='${x2}'][data-y='${y}']`
-    );
+    if (row1) {
+      wall1 = document.querySelector(
+        `.cell[data-column='${column}'][data-row='${row1}}']`
+      );
+    }
+    console.log(wall1);
+    if (row2) {
+      wall2 = document.querySelector(
+        `.cell[data-column='${column}'][data-row='${row2}']`
+      );
+    }
+    console.log(wall2);
+    if (column1) {
+      wall3 = document.querySelector(
+        `.cell[data-column='${column1}'][data-row='${row}']`
+      );
+    }
+    console.log(wall3);
+    if (column2) {
+      wall4 = document.querySelector(
+        `.cell[data-column='${column2}'][data-row='${row}']`
+      );
+    }
+    console.log(wall4);
 
-    if (!wall1?.classList.contains('set'))
+    if (row3 && wall1 && !wall1?.classList.contains('set'))
       options.push(
         document.querySelector(
-          `.cell[data-x='${x}'][data-y='${oy1}']`
+          `.cell[data-column='${column}'][data-row='${row3}']`
         ) as HTMLElement
       );
-    if (!wall2?.classList.contains('set'))
+    if (row4 && wall2 && !wall2?.classList.contains('set'))
       options.push(
         document.querySelector(
-          `.cell[data-x='${x}'][data-y='${oy2}']`
+          `.cell[data-column='${column}'][data-row='${row4}']`
         ) as HTMLElement
       );
-    if (!wall3?.classList.contains('set'))
+    if (column3 && wall3 && !wall3?.classList.contains('set'))
       options.push(
         document.querySelector(
-          `.cell[data-x='${ox1}'][data-y='${y}']`
+          `.cell[data-column='${column3}'][data-row='${row}']`
         ) as HTMLElement
       );
-    if (!wall4?.classList.contains('set'))
+    if (column4 && wall4 && !wall4?.classList.contains('set'))
       options.push(
         document.querySelector(
-          `.cell[data-x='${ox2}'][data-y='${y}']`
+          `.cell[data-column='${column4}'][data-row='${row}']`
         ) as HTMLElement
       );
+    console.log(options);
 
     for (let i = 0; i < options.length; i++) {
       options[i].style.backgroundColor === 'rgb(0, 0, 128)'
@@ -167,55 +168,55 @@ document.getElementById('game_container')?.addEventListener('click', e => {
     direction = Direction.Down;
     target.classList.add('set');
     document
-      .querySelector(`.cell[data-x='${x}'][data-y='${y2}']`)
+      .querySelector(`.cell[data-column='${column}'][data-row='${row2}']`)
       ?.classList.add('wallv');
     document
-      .querySelector(`.cell[data-x='${x}'][data-y='${y2}']`)
+      .querySelector(`.cell[data-column='${column}'][data-row='${row2}']`)
       ?.classList.add('set');
     document
-      .querySelector(`.cell[data-x='${x}'][data-y='${y3}']`)
+      .querySelector(`.cell[data-column='${column}'][data-row='${row4}']`)
       ?.classList.add('set');
   } else if (target.classList.contains('wallh')) {
     direction = Direction.Right;
     target.classList.add('set');
     document
-      .querySelector(`.cell[data-x='${x2}'][data-y='${y}']`)
+      .querySelector(`.cell[data-column='${column2}'][data-row='${row}']`)
       ?.classList.add('wallh');
     document
-      .querySelector(`.cell[data-x='${x2}'][data-y='${y}']`)
+      .querySelector(`.cell[data-column='${column2}'][data-row='${row}']`)
       ?.classList.add('set');
     document
-      .querySelector(`.cell[data-x='${x3}'][data-y='${y}']`)
+      .querySelector(`.cell[data-column='${column4}'][data-row='${row}']`)
       ?.classList.add('set');
   }
-  emitter.emit(PLAYER_ACTION_EVENT, Point.create(x, y), direction);
+  emitter.emit(PLAYER_ACTION_EVENT, Point.create(column, row), direction);
 });
 
 document.getElementById('game_container')?.addEventListener('mouseover', e => {
   const target: HTMLElement = e.target as HTMLElement;
-  const x = +(target.getAttribute('data-x') as string);
-  const y = +(target.getAttribute('data-y') as string);
-  const y1 = y + 1;
-  const y2 = y + 2;
-  const x1 = x + 1;
-  const x2 = x + 2;
+  const column = +(target.getAttribute('data-column') as string);
+  const row = +(target.getAttribute('data-row') as string);
+  const row1 = row + 1;
+  const row2 = row + 2;
+  const column1 = column + 1;
+  const column2 = column + 2;
 
   const p1 = document.querySelector(
-    `.cell[data-x='${x}'][data-y='${y1}']`
+    `.cell[data-column='${column}'][data-row='${row1}']`
   ) as HTMLElement;
   const p2 = document.querySelector(
-    `.cell[data-x='${x}'][data-y='${y2}']`
+    `.cell[data-column='${column}'][data-row='${row2}']`
   ) as HTMLElement;
   const p3 = document.querySelector(
-    `.cell[data-x='${x1}'][data-y='${y}']`
+    `.cell[data-column='${column1}'][data-row='${row}']`
   ) as HTMLElement;
   const p4 = document.querySelector(
-    `.cell[data-x='${x2}'][data-y='${y}']`
+    `.cell[data-column='${column2}'][data-row='${row}']`
   ) as HTMLElement;
 
   if (
     target.classList.contains('wallv') &&
-    y !== 16 &&
+    row !== 16 &&
     !target.classList.contains('set') &&
     !p1.classList.contains('set') &&
     !p2.classList.contains('set')
@@ -225,7 +226,7 @@ document.getElementById('game_container')?.addEventListener('mouseover', e => {
     p2.style.backgroundColor = '#000080';
   } else if (
     target.classList.contains('wallh') &&
-    x !== 16 &&
+    column !== 16 &&
     !target.classList.contains('set') &&
     !p3.classList.contains('set') &&
     !p4.classList.contains('set')
@@ -239,24 +240,24 @@ document.getElementById('game_container')?.addEventListener('mouseover', e => {
 document.getElementById('game_container')?.addEventListener('mouseout', e => {
   const target = e.target as HTMLElement;
 
-  const x = +(target.getAttribute('data-x') as string);
-  const y = +(target.getAttribute('data-y') as string);
-  const y1 = y + 1;
-  const y2 = y + 2;
-  const x1 = x + 1;
-  const x2 = x + 2;
+  const column = +(target.getAttribute('data-column') as string);
+  const row = +(target.getAttribute('data-row') as string);
+  const row1 = row + 1;
+  const row2 = row + 2;
+  const column1 = column + 1;
+  const column2 = column + 2;
 
   const p1 = document.querySelector(
-    `.cell[data-x='${x}'][data-y='${y1}']`
+    `.cell[data-column='${column}'][data-row='${row1}']`
   ) as HTMLElement;
   const p2 = document.querySelector(
-    `.cell[data-x='${x}'][data-y='${y2}']`
+    `.cell[data-column='${column}'][data-row='${row2}']`
   ) as HTMLElement;
   const p3 = document.querySelector(
-    `.cell[data-x='${x1}'][data-y='${y}']`
+    `.cell[data-column='${column1}'][data-row='${row}']`
   ) as HTMLElement;
   const p4 = document.querySelector(
-    `.cell[data-x='${x2}'][data-y='${y}']`
+    `.cell[data-column='${column2}'][data-row='${row}']`
   ) as HTMLElement;
   if (
     target.classList.contains('wallv') &&
