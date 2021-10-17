3c65c6c2270e98db34160ab5e3134af8d8bc37a8
Yuliia Tkachenko <yulia032022@gmail.com>
2021-10-17 15:09:10 +0300

    fix titles for players walls count

--- src/web/enums/walls-count-title.ts
+++ src/web/enums/walls-count-title.ts
@@ -1,5 +1,5 @@
 export enum wallsCountTitle {
   COMPUTER = 'Computer walls: ',
-  OTHER_PLAYER = 'Second player walls: ',
-  CURRENT_PLAYER = 'First player walls: ',
+  OTHER_PLAYER = 'First player walls: ',
+  CURRENT_PLAYER = 'Second player walls: ',
 }
