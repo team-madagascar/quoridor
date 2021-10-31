0f38fa9dd934a3df7a878bd7eb14a9f925adfa06
Yuliia Tkachenko <yulia032022@gmail.com>
2021-10-31 13:41:17 +0200

    add estimate function

---
+++ src/console/estimate-function.ts
@@ -0,0 +1,22 @@
+import {GameView} from '../domain/core/game';
+
+export const estimateFunction = (game: GameView) => {
+  const currentPlayer = game.currentPlayer;
+  const currentOpponent = game.currentOpponent;
+
+  const currentPlayerPositionNode = game.getNode(currentPlayer.currentPosition);
+  const opponentPositionNode = game.getNode(currentOpponent.currentPosition);
+
+  const currentPlayerShortestDistance: number =
+    currentPlayerPositionNode.shortestDistanceTo(
+      n => currentPlayer.finishRow === n.position.row
+    ) as number;
+  const opponentShortestDistance: number =
+    opponentPositionNode.shortestDistanceTo(
+      n => currentOpponent.finishRow === n.position.row
+    ) as number;
+
+  const estimate = opponentShortestDistance - currentPlayerShortestDistance;
+
+  return estimate;
+};
